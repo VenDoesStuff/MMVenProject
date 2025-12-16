5bcc7ab7150ccf5eda50e1bfae032d748e626c6a
VenDoesStuff <VentusDoesStuff@gmail.com>
2025-12-15 23:03:41 -0700

    added adult, changing main c

---
+++ src/AdultHands.c
@@ -0,0 +1,117 @@
+#include "global.h"
+
+Vtx gLinkAdultLeftHandClosedNearVtx[] = {
+    VTX(  -172,     23,    -92,    0x47,    0x18, 0xBC, 0xB6, 0xBF, 0xFF), // 0
+    VTX(  -132,     22,    135,   0x1BF,    0x45, 0xD6, 0x9F, 0x38, 0xFF), // 1
+    VTX(  -218,    210,    230,   0x1ED,   0x175, 0xC5, 0xCC, 0x5B, 0xFF), // 2
+    VTX(  -308,    218,    -15,    0x36,   0x173, 0x8B, 0x05, 0xE6, 0xFF), // 3
+    VTX(   204,    385,   -302,  -0x134,   0x166, 0x30, 0x27, 0x9A, 0xFF), // 4
+    VTX(   302,    165,   -266,  -0x152,    0xB0, 0x42, 0xBC, 0xB7, 0xFF), // 5
+    VTX(  -172,     23,    -92,   0x1CF,    0x92, 0xBC, 0xB6, 0xBF, 0xFF), // 6
+    VTX(   105,      3,   -141,    0x3E,    0x59, 0x10, 0x93, 0xD1, 0xFF), // 7
+    VTX(   145,      2,     86,   0x1CA,    0x2B, 0x21, 0x94, 0x29, 0xFF), // 8
+    VTX(   105,      3,   -141,    0xCF,     0xF, 0x10, 0x93, 0xD1, 0xFF), // 9
+    VTX(   302,    165,   -266,    0x33,   0x173, 0x42, 0xBC, 0xB7, 0xFF), // 10
+    VTX(   315,    171,    177,   0x21F,   0x171, 0x55, 0xE2, 0x4F, 0xFF), // 11
+    VTX(   204,    385,   -302,   0x1BA,   0x1BE, 0x30, 0x27, 0x9A, 0xFF), // 12
+    VTX(   315,    171,    177,    0xC3,     0x1, 0x55, 0xE2, 0x4F, 0xFF), // 13
+    VTX(   302,    165,   -266,   0x22F,    0xBD, 0x42, 0xBC, 0xB7, 0xFF), // 14
+    VTX(  -308,    218,    -15,   0x268,   0x149, 0x8B, 0x05, 0xE6, 0xFF), // 15
+    VTX(   -68,    311,    -91,    0xCF,   0x16A, 0xC0, 0x23, 0xA1, 0xFF), // 16
+    VTX(   315,    171,    177,   0x19B,   0x168, 0x55, 0xE2, 0x4F, 0xFF), // 17
+    VTX(   204,    385,   -302,   0x4E2,   0x404, 0x30, 0x27, 0x9A, 0xFF), // 18
+    VTX(   295,    483,    226,     0x0,   0x400, 0x61, 0x33, 0x30, 0xFF), // 19
+    VTX(  -308,    218,    -15,   0x1D5,   0x106, 0x8B, 0x05, 0xE6, 0xFF), // 20
+    VTX(  -218,    210,    230,    -0x8,   0x15B, 0xC5, 0xCC, 0x5B, 0xFF), // 21
+    VTX(  -166,    527,    262,     0x6,   0x3ED, 0xD1, 0x32, 0x62, 0xFF), // 22
+    VTX(   145,      2,     86,     0x9,   0x3CF, 0x21, 0x94, 0x29, 0xFF), // 23
+    VTX(  -132,     22,    135,   0x1F8,   0x3D9, 0xD6, 0x9F, 0x38, 0xFF), // 24
+    VTX(  -172,     23,    -92,   0x1F7,   0x1BE, 0xBC, 0xB6, 0xBF, 0xFF), // 25
+    VTX(   105,      3,   -141,     0x8,   0x1B3, 0x10, 0x93, 0xD1, 0xFF), // 26
+    VTX(  -218,    210,    230,   0x21A,   0x150, 0xC5, 0xCC, 0x5B, 0xFF), // 27
+    VTX(  -132,     22,    135,   0x1A3,   -0x43, 0xD6, 0x9F, 0x38, 0xFF), // 28
+    VTX(   145,      2,     86,    0x6E,   -0x3E, 0x21, 0x94, 0x29, 0xFF), // 29
+    VTX(  -166,    527,    262,   0x200,   0x400, 0xD1, 0x32, 0x62, 0xFF), // 30
+    VTX(   315,    171,    177,   -0x36,   0x167, 0x55, 0xE2, 0x4F, 0xFF), // 31
+    VTX(   295,    483,    226,     0x0,   0x400, 0x61, 0x33, 0x30, 0xFF), // 32
+    VTX(   156,    679,   -119,   0x1E1,   0x16D, 0xF5, 0x71, 0xDB, 0xFF), // 33
+    VTX(   204,    385,   -302,    0x41,   0x1D5, 0x30, 0x27, 0x9A, 0xFF), // 34
+    VTX(   -68,    311,    -91,    0x39,   -0x8C, 0xC0, 0x23, 0xA1, 0xFF), // 35
+    VTX(   204,    385,   -302,   0x1FF,   0x1F6, 0x30, 0x27, 0x9A, 0xFF), // 36
+    VTX(   156,    679,   -119,    0x13,   0x1EF, 0xF5, 0x71, 0xDB, 0xFF), // 37
+    VTX(   295,    483,    226,    0x36,    -0x7, 0x61, 0x33, 0x30, 0xFF), // 38
+    VTX(   -68,    311,    -91,   0x149,   -0x89, 0xC0, 0x23, 0xA1, 0xFF), // 39
+    VTX(  -308,    218,    -15,   0x1E0,   -0xF1, 0x8B, 0x05, 0xE6, 0xFF), // 40
+    VTX(  -217,    562,    130,   0x1C6,   0x179, 0xAA, 0x4B, 0xDC, 0xFF), // 41
+    VTX(  -166,    527,    262,   0x140,   0x233, 0xD1, 0x32, 0x62, 0xFF), // 42
+    VTX(  -217,    562,    130,   0x1E2,   0x1F0, 0xAA, 0x4B, 0xDC, 0xFF), // 43
+    VTX(  -308,    218,    -15,   0x1AB,  -0x18C, 0x8B, 0x05, 0xE6, 0xFF), // 44
+    VTX(   156,    679,   -119,    0x45,    0xC5, 0xF5, 0x71, 0xDB, 0xFF), // 45
+    VTX(  -166,    527,    262,   0x1C4,   0x179, 0xD1, 0x32, 0x62, 0xFF), // 46
+    VTX(   295,    483,    226,    0x42,   0x1B3, 0x61, 0x33, 0x30, 0xFF), // 47
+    VTX(  -217,    562,    130,   0x1C6,   0x128, 0xAA, 0x4B, 0xDC, 0xFF), // 48
+    VTX(   156,    679,   -119,    0x47,   0x115, 0xF5, 0x71, 0xDB, 0xFF), // 49
+};
+
+u64 gVentusAdultSkelManual_GlovesandBelt_ci4[] = {
+	0x0000000000000000, 0x0000000000000000, 0x0000000000000000, 0x0000000000000000, 0x0000000000000000, 0x0000000000000000, 0x0000000000000000, 0x0000000000000000, 
+	0x0000000000000000, 0x0000000000000000, 0x0000000000000000, 0x0000000000000000, 0x0000000000000000, 0x0000000000000000, 0x0000000000000000, 0x0000000000000000, 
+};
+
+u64 gVentusAdultSkelManual_GlovesandBelt_pal_rgba16[] = {
+	0x30c1000000000000
+};
+
+Gfx LeftHandBigDL[] = {
+    gsDPPipeSync(),
+    gsDPSetTextureLUT(G_TT_RGBA16),
+    gsSPTexture(0xFFFF, 0xFFFF, 0, G_TX_RENDERTILE, G_ON),
+    gsDPLoadTextureBlock(gVentusAdultSkelManual_GlovesandBelt_ci4, G_IM_FMT_CI, G_IM_SIZ_16b, 16,
+ 16, 0, G_TX_MIRROR | G_TX_WRAP, G_TX_MIRROR | G_TX_WRAP, 4, 5, G_TX_NOLOD,
+ G_TX_NOLOD),
+    gsDPLoadTLUT_pal256(gVentusAdultSkelManual_GlovesandBelt_pal_rgba16),
+    gsDPSetCombineLERP(TEXEL0, 0, SHADE, 0, 0, 0, 0, 1, COMBINED, 0, PRIMITIVE, 0, 0, 0, 0, COMBINED),
+    gsDPSetRenderMode(G_RM_FOG_SHADE_A, G_RM_AA_ZB_OPA_SURF2),
+    gsSPClearGeometryMode(G_TEXTURE_GEN | G_TEXTURE_GEN_LINEAR),
+    gsSPSetGeometryMode(G_FOG | G_LIGHTING),
+    gsSPDisplayList(0x0C000000),
+    gsDPSetPrimColor(0, 0, 255, 255, 255, 255),
+    gsSPVertex(&gLinkAdultLeftHandClosedNearVtx[0], 23, 0),
+    gsSP2Triangles(0, 1, 2, 0, 0, 2, 3, 0),
+    gsSP2Triangles(4, 5, 6, 0, 7, 6, 5, 0),
+    gsSP2Triangles(8, 9, 10, 0, 11, 8, 10, 0),
+    gsSP2Triangles(12, 13, 14, 0, 6, 15, 16, 0),
+    gsSP2Triangles(17, 18, 19, 0, 6, 16, 4, 0),
+    gsSP1Triangle(20, 21, 22, 0),
+    gsDPPipeSync(),
+    gsDPLoadTextureBlock(gVentusAdultSkelManual_GlovesandBelt_ci4, G_IM_FMT_CI, G_IM_SIZ_16b, 16,
+ 16, 0, G_TX_MIRROR | G_TX_WRAP, G_TX_MIRROR | G_TX_WRAP, 4, 5, G_TX_NOLOD,
+ G_TX_NOLOD),
+    gsSPVertex(&gLinkAdultLeftHandClosedNearVtx[23], 10, 0),
+    gsSP2Triangles(0, 1, 2, 0, 0, 2, 3, 0),
+    gsSP2Triangles(4, 5, 6, 0, 7, 8, 9, 0),
+    gsSP2Triangles(8, 7, 4, 0, 8, 4, 6, 0),
+    gsDPPipeSync(),
+    gsDPLoadTextureBlock(gVentusAdultSkelManual_GlovesandBelt_ci4, G_IM_FMT_CI, G_IM_SIZ_16b, 16,
+ 16, 0, G_TX_NOMIRROR | G_TX_CLAMP, G_TX_NOMIRROR | G_TX_CLAMP, 4, 4, G_TX_NOLOD,
+ G_TX_NOLOD),
+    gsDPLoadTLUT_pal256(gVentusAdultSkelManual_GlovesandBelt_pal_rgba16),
+    gsSPVertex(&gLinkAdultLeftHandClosedNearVtx[33], 3, 0),
+    gsSP1Triangle(0, 1, 2, 0),
+    gsDPPipeSync(),
+    gsDPLoadTextureBlock(gVentusAdultSkelManual_GlovesandBelt_ci4, G_IM_FMT_CI, G_IM_SIZ_16b, 16,
+ 16, 0, G_TX_NOMIRROR | G_TX_WRAP, G_TX_NOMIRROR | G_TX_WRAP, 4, 4, G_TX_NOLOD,
+ G_TX_NOLOD),
+    gsSPVertex(&gLinkAdultLeftHandClosedNearVtx[36], 3, 0),
+    gsSP1Triangle(0, 1, 2, 0),
+    gsDPPipeSync(),
+    gsDPLoadTextureBlock(gVentusAdultSkelManual_GlovesandBelt_ci4, G_IM_FMT_CI, G_IM_SIZ_16b, 16,
+ 16, 0, G_TX_NOMIRROR | G_TX_CLAMP, G_TX_NOMIRROR | G_TX_CLAMP, 4, 4, G_TX_NOLOD, G_TX_NOLOD),
+    gsDPSetCombineLERP(TEXEL0, 0, SHADE, 0, 0, 0, 0, TEXEL0, COMBINED, 0, PRIMITIVE, 0, 0, 0, 0, COMBINED),
+    gsDPSetRenderMode(G_RM_FOG_SHADE_A, G_RM_AA_ZB_TEX_EDGE2),
+    gsSPVertex(&gLinkAdultLeftHandClosedNearVtx[39], 11, 0),
+    gsSP2Triangles(0, 1, 2, 0, 3, 4, 5, 0),
+    gsSP2Triangles(6, 7, 8, 0, 9, 7, 6, 0),
+    gsSP1Triangle(0, 2, 10, 0),
+    gsSPEndDisplayList(),
+};
--- src/main.c
+++ src/main.c
@@ -17,9 +17,23 @@
 #include "gVentusMirrorDL.h"
 #include "gVentusMirrorRayDL.h"
 #include "gVentusMirrorRayBeamDL.h"
+#include "gVentusDekuSkel.h"
+#include "gVentusAdultSkel.h"
+#include "AdultHands.h"
 
 extern TexturePtr eyeTextures[];
 extern TexturePtr mouthTextures[];
+
+PLAYERMODELMANAGER_CALLBACK_REGISTER_MODELS void addVentustoModelManager() {
+
+    PlayerModelManagerHandle childPack = PLAYERMODELMANAGER_REGISTER_MODEL("VenChildPack", PMM_MODEL_TYPE_MODEL_PACK);
+    PlayerModelManager_setAuthor(childPack, "VenDoesStuff");
+    PlayerModelManager_setDisplayName(childPack, "Ventus (Child Sized)");
+
+    PlayerModelManagerHandle adultPack = PLAYERMODELMANAGER_REGISTER_MODEL("VenBigPack", PMM_MODEL_TYPE_MODEL_PACK);
+    PlayerModelManager_setAuthor(adultPack, "VenDoesStuff");
+    PlayerModelManager_setDisplayName(adultPack, "Ventus (Adult Sized)");
+}
 
 PLAYERMODELMANAGER_CALLBACK_REGISTER_MODELS void registerVen() {
     // PlayerModelManager stuff
@@ -39,10 +53,30 @@ PLAYERMODELMANAGER_CALLBACK_REGISTER_MODELS void registerVen() {
     PlayerModelManager_setDisplayList(h, PMM_DL_FPS_RFOREARM, gVentusMiniSkel_bone017_gLinkChildRightForearmLimb_mesh_layer_Opaque);
     PlayerModelManager_setDisplayList(h, PMM_DL_FPS_LHAND, gVentusMiniLeftHandClosedDL_mesh);
     PlayerModelManager_setDisplayList(h, PMM_DL_FPS_LFOREARM, gVentusMiniSkel_bone014_gLinkChildLeftForearmLimb_mesh_layer_Opaque);
+
+}
+
+PLAYERMODELMANAGER_CALLBACK_REGISTER_MODELS void registerVenBig() {
+    // PlayerModelManager stuff
+    PlayerModelManagerHandle h = PLAYERMODELMANAGER_REGISTER_MODEL("mmVenbig", PMM_MODEL_TYPE_ADULT);
+
+    PlayerModelManager_setAuthor(h, "VenDoesStuff");
+    PlayerModelManager_setDisplayName(h, "Ventus (big)");
+    // PlayerModelManager_setEyesTextures(h, eyeTextures);
+    // PlayerModelManager_setMouthTextures(h, mouthTextures);
+
+    PlayerModelManager_setSkeleton(h, &gVentusAdultSkel);
+
+    PlayerModelManager_setDisplayList(h, PMM_DL_LFIST, LeftHandBigDL);
+    // PlayerModelManager_setDisplayList(h, PMM_DL_RFIST, gVentusMiniRightHandClosedDL_mesh);
+    // PlayerModelManager_setDisplayList(h, PMM_DL_LHAND_BOTTLE, gVentusMiniLeftHandClosedDL_mesh);
+    // PlayerModelManager_setDisplayList(h, PMM_DL_FPS_RHAND, gVentusMiniRightHandClosedDL_mesh);
+    // PlayerModelManager_setDisplayList(h, PMM_DL_FPS_RFOREARM, gVentusMiniSkel_bone017_gLinkChildRightForearmLimb_mesh_layer_Opaque);
+    // PlayerModelManager_setDisplayList(h, PMM_DL_FPS_LHAND, gVentusMiniLeftHandClosedDL_mesh);
+    // PlayerModelManager_setDisplayList(h, PMM_DL_FPS_LFOREARM, gVentusMiniSkel_bone014_gLinkChildLeftForearmLimb_mesh_layer_Opaque);
 }
 	
 PLAYERMODELMANAGER_CALLBACK_REGISTER_MODELS void registerVenKokiri() {
-	
 	PlayerModelManagerHandle h = PLAYERMODELMANAGER_REGISTER_MODEL("mmVenKokiri", PMM_MODEL_TYPE_SWORD_KOKIRI);
 	
     PlayerModelManager_setDisplayName(h, "Norende Sword");
@@ -53,7 +87,6 @@ PLAYERMODELMANAGER_CALLBACK_REGISTER_MODELS void registerVenKokiri() {
 }
 
 PLAYERMODELMANAGER_CALLBACK_REGISTER_MODELS void registerVenGilded() {
-	
 	PlayerModelManagerHandle h = PLAYERMODELMANAGER_REGISTER_MODEL("mmVenGilded", PMM_MODEL_TYPE_SWORD_GILDED);
 	
     PlayerModelManager_setDisplayName(h, "Geneolgia Rapier");
@@ -64,7 +97,6 @@ PLAYERMODELMANAGER_CALLBACK_REGISTER_MODELS void registerVenGilded() {
 }
 
 PLAYERMODELMANAGER_CALLBACK_REGISTER_MODELS void registerVenGFS() {
-	
 	PlayerModelManagerHandle h = PLAYERMODELMANAGER_REGISTER_MODEL("mmVenGFS", PMM_MODEL_TYPE_SWORD_GREAT_FAIRY);
 	
     PlayerModelManager_setDisplayName(h, "Geneolgia Axe");
@@ -73,7 +105,6 @@ PLAYERMODELMANAGER_CALLBACK_REGISTER_MODELS void registerVenGFS() {
 }
 
 PLAYERMODELMANAGER_CALLBACK_REGISTER_MODELS void registerVenHerosShield() {
-
     PlayerModelManagerHandle h = PLAYERMODELMANAGER_REGISTER_MODEL("mmVenHerosShield", PMM_MODEL_TYPE_SHIELD_HERO);
 
     PlayerModelManager_setDisplayName(h, "Norende Wooden Shield");
@@ -82,7 +113,6 @@ PLAYERMODELMANAGER_CALLBACK_REGISTER_MODELS void registerVenHerosShield() {
 }
 
 PLAYERMODELMANAGER_CALLBACK_REGISTER_MODELS void registerVenMirrorShield() {
-
     PlayerModelManagerHandle h = PLAYERMODELMANAGER_REGISTER_MODEL("mmVenMirrorShield", PMM_MODEL_TYPE_SHIELD_MIRROR);
 
     PlayerModelManager_setDisplayName(h, "Geneolgia Shield");
@@ -90,4 +120,95 @@ PLAYERMODELMANAGER_CALLBACK_REGISTER_MODELS void registerVenMirrorShield() {
     PlayerModelManager_setDisplayList(h, PMM_DL_SHIELD_MIRROR, gVentusMirrorDL);
     PlayerModelManager_setDisplayList(h, PMM_DL_SHIELD_MIRROR_RAY, gVentusMirrorRayDL);
     PlayerModelManager_setDisplayList(h, PMM_DL_SHIELD_MIRROR_RAY_BEAM, gVentusMirrorRayBeamDL);
+}
+
+PLAYERMODELMANAGER_CALLBACK_REGISTER_MODELS void registerVenDeku() {
+    PlayerModelManagerHandle h = PLAYERMODELMANAGER_REGISTER_MODEL("mmVenDeku", PMM_MODEL_TYPE_DEKU);
+
+    PlayerModelManager_setAuthor(h, "VenDoesStuff");
+    PlayerModelManager_setDisplayName(h, "Ventus");
+
+    PlayerModelManager_setSkeleton(h, &gVentusDekuSkel);
+
+    PlayerModelManager_setDisplayList(h, PMM_DL_LFIST, gVentusDekuSkel_bone015_gLinkDekuLeftHandLimb_mesh_layer_Opaque);
+    PlayerModelManager_setDisplayList(h, PMM_DL_RFIST, gVentusDekuSkel_bone018_gLinkDekuRightHandLimb_mesh_layer_Opaque);
+    PlayerModelManager_setDisplayList(h, PMM_DL_LHAND_BOTTLE, gVentusDekuSkel_bone015_gLinkDekuLeftHandLimb_mesh_layer_Opaque);
+}
+
+#define DECLARE_MATRIX_WRAPPED_DL(dlName, mtxName, dl)                  \
+    Mtx mtxName;                                                        \
+    Gfx dlName[] = {                                                    \
+        gsSPMatrix(&mtxName, G_MTX_PUSH | G_MTX_MUL | G_MTX_MODELVIEW), \
+        gsSPDisplayList(dl),                                            \
+        gsSPPopMatrix(G_MTX_MODELVIEW),                                 \
+        gsSPEndDisplayList(),                                           \
+    }
+
+DECLARE_MATRIX_WRAPPED_DL(gVentusHeroesBigDL, gVentusHeroesBigMtx, gVentusHeroesDL_mesh);
+DECLARE_MATRIX_WRAPPED_DL(gVentusMirrorBigDL, gVentusMirrorBigMtx, gVentusMirrorDL);
+DECLARE_MATRIX_WRAPPED_DL(gVentusMirrorRayBigDL, gVentusMirrorRayBigMtx, gVentusMirrorRayDL);
+DECLARE_MATRIX_WRAPPED_DL(gVentusMirrorRayBeamBigDL, gVentusMirrorRayBeamBigMtx, gVentusMirrorRayBeamDL);
+DECLARE_MATRIX_WRAPPED_DL(gVentusGildedBladeBigDL, gVentusGildedBladeBigMtx, gVentusGildedBladeDL_mesh);
+DECLARE_MATRIX_WRAPPED_DL(gVentusGildedHandleBigDL, gVentusGildedHandleBigMtx, gVentusGildedHandleDL_mesh);
+DECLARE_MATRIX_WRAPPED_DL(gVentusGildedSheathBigDL, gVentusGildedSheathBigMtx, gVentusGildedSheathDL_mesh);
+DECLARE_MATRIX_WRAPPED_DL(gVentusKokiriBladeBigDL, gVentusKokiriBladeBigMtx, gVentusKokiriBladeDL_mesh);
+DECLARE_MATRIX_WRAPPED_DL(gVentusKokiriHandleBigDL, gVentusKokiriHandleBigMtx, gVentusKokiriHandleDL_mesh);
+DECLARE_MATRIX_WRAPPED_DL(gVentusKokiriSheathBigDL, gVentusKokiriSheathBigMtx, gVentusKokiriSheathDL_mesh);
+
+PLAYERMODELMANAGER_CALLBACK_REGISTER_MODELS void registerVenHerosShieldBig() {
+    PlayerModelManagerHandle h = PLAYERMODELMANAGER_REGISTER_MODEL("mmVenHerosShieldBig", PMM_MODEL_TYPE_SHIELD_HERO);
+
+    PlayerModelManager_setDisplayName(h, "Norende Wooden Shield (Big)");
+    PlayerModelManager_setAuthor(h, "VenDoesStuff");
+    guScale(&gVentusHeroesBigMtx, 1.25f, 1.25f, 1.25f); // scale matrix by 1.25 (approx. child > adult)
+    PlayerModelManager_setDisplayList(h, PMM_DL_SHIELD_HERO, gVentusHeroesBigDL);
+}
+
+PLAYERMODELMANAGER_CALLBACK_REGISTER_MODELS void registerVenMirrorShieldBig() {
+    PlayerModelManagerHandle h = PLAYERMODELMANAGER_REGISTER_MODEL("mmVenMirrorShieldBig", PMM_MODEL_TYPE_SHIELD_MIRROR);
+
+    PlayerModelManager_setDisplayName(h, "Geneolgia Shield (Big)");
+    PlayerModelManager_setAuthor(h, "VenDoesStuff");
+    guScale(&gVentusMirrorBigMtx, 1.25f, 1.25f, 1.25f); // scale matrix by 1.25 (approx. child > adult)
+    guScale(&gVentusMirrorRayBigMtx, 1.25f, 1.25f, 1.25f); // scale matrix by 1.25 (approx. child > adult)
+    guScale(&gVentusMirrorRayBeamBigMtx, 1.25f, 1.25f, 1.25f); // scale matrix by 1.25 (approx. child > adult)
+    PlayerModelManager_setDisplayList(h, PMM_DL_SHIELD_MIRROR, gVentusMirrorBigDL);
+    PlayerModelManager_setDisplayList(h, PMM_DL_SHIELD_MIRROR_RAY, gVentusMirrorRayBigDL);
+    PlayerModelManager_setDisplayList(h, PMM_DL_SHIELD_MIRROR_RAY_BEAM, gVentusMirrorRayBeamBigDL);
+}
+
+PLAYERMODELMANAGER_CALLBACK_REGISTER_MODELS void registerVenGildedBig() {
+    PlayerModelManagerHandle h = PLAYERMODELMANAGER_REGISTER_MODEL("mmVenGildedBig", PMM_MODEL_TYPE_SWORD_GILDED);
+	
+    PlayerModelManager_setDisplayName(h, "Geneolgia Rapier (Big)");
+    PlayerModelManager_setAuthor(h, "VenDoesStuff");
+    guScale(&gVentusGildedBladeBigMtx, 1.25f, 1.25f, 1.25f); // scale matrix by 1.25 (approx. child > adult)
+    guScale(&gVentusGildedHandleBigMtx, 1.25f, 1.25f, 1.25f); // scale matrix by 1.25 (approx. child > adult)
+    guScale(&gVentusGildedSheathBigMtx, 1.25f, 1.25f, 1.25f); // scale matrix by 1.25 (approx. child > adult)
+    PlayerModelManager_setDisplayList(h, PMM_DL_SWORD_GILDED_BLADE, gVentusGildedBladeBigDL);
+    PlayerModelManager_setDisplayList(h, PMM_DL_SWORD_GILDED_HILT, gVentusGildedHandleBigDL);
+    PlayerModelManager_setDisplayList(h, PMM_DL_SWORD_GILDED_SHEATH, gVentusGildedSheathBigDL);
+    
+    // reposition handle on link's back
+    static Mtx swordBackRepositionMtx;
+    guPosition(&swordBackRepositionMtx, 0.f, 0.f, 0.f, 1.f, -715.f, -260.f, -50.f);
+    PlayerModelManager_setMatrix(h, PMM_MTX_SWORD_GILDED_BACK, &swordBackRepositionMtx);
+}
+
+PLAYERMODELMANAGER_CALLBACK_REGISTER_MODELS void registerVenKokiriBig() {
+	PlayerModelManagerHandle h = PLAYERMODELMANAGER_REGISTER_MODEL("mmVenKokiriBig", PMM_MODEL_TYPE_SWORD_KOKIRI);
+	
+    PlayerModelManager_setDisplayName(h, "Norende Sword (Big)");
+    PlayerModelManager_setAuthor(h, "VenDoesStuff");
+    guScale(&gVentusKokiriBladeBigMtx, 1.25f, 1.25f, 1.25f); // scale matrix by 1.25 (approx. child > adult)
+    guScale(&gVentusKokiriHandleBigMtx, 1.25f, 1.25f, 1.25f); // scale matrix by 1.25 (approx. child > adult)
+    guScale(&gVentusKokiriSheathBigMtx, 1.25f, 1.25f, 1.25f); // scale matrix by 1.25 (approx. child > adult)
+    PlayerModelManager_setDisplayList(h, PMM_DL_SWORD_KOKIRI_BLADE, gVentusKokiriBladeBigDL);
+    PlayerModelManager_setDisplayList(h, PMM_DL_SWORD_KOKIRI_HILT, gVentusKokiriHandleBigDL);
+    PlayerModelManager_setDisplayList(h, PMM_DL_SWORD_KOKIRI_SHEATH, gVentusKokiriSheathBigDL);
+
+    // reposition handle on link's back
+    static Mtx swordBackRepositionMtx;
+    guPosition(&swordBackRepositionMtx, 0.f, 0.f, 0.f, 1.f, -715.f, -310.f, -50.f);
+    PlayerModelManager_setMatrix(h, PMM_MTX_SWORD_KOKIRI_BACK, &swordBackRepositionMtx);
 }
